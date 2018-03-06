8b1c9549befd1e3085a72d930df4bf11a4ad150f
oscar-hakkinen <Hako1_15@uni.worc.ac.uk>
2018-03-06 15:34:05 +0000

    syncro test

--- js/index.js
+++ js/index.js
@@ -5,9 +5,8 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	
 	// Use an HTML GET request to obtain data from an API
 	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", feedURL, false);
-	xmlhttp.send();
-		
+	   xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
 		
 	// parse the resulting JSON into Javascript Data Object 
 	// you can use a live parser to inspect the contents of the JSON
@@ -21,5 +20,10 @@ $(document).on('pagecreate', '#feedPage', function(event) {
     	template: '#myTemplate', <!-- how -->
     	data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
 	});
+  };
+};
+    xmlhttp.open("GET", feedURL, true);
+	xmlhttp.send();
+
 	
 });
